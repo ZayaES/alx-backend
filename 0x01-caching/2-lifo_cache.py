51ec283d8d67060b5fbccad0178c84c0099c8feb
Zaya <heavenphrince@gmail.com>
2024-02-01 05:22:04 +0100

    2 1st incomplete

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -6,7 +6,7 @@ from typing import Dict, Any
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class FIFOCache(BaseCaching):
+class LIFOCache(BaseCaching):
     """class that hold required module for FIFO caching"""
 
     def __init__(self):
@@ -22,9 +22,9 @@ class FIFOCache(BaseCaching):
             return
         my_dict[key] = item
         if len(my_dict) > 4:
-            first_in = list(my_dict.keys())[0]
-            del my_dict[first_in]
-            print("DISCARD: {}".format(first_in))
+            last_in = list(my_dict.keys())[-2]
+            del my_dict[last_in]
+            print("DISCARD: {}".format(last_in))
 
     def get(self, key: Any) -> Any:
         """returns the value for the key"""
